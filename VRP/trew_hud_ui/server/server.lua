e4b52f974a90e228064988cfc104b982e998ebb2
vortisrd <xrodrigolima@gmail.com>
2019-11-27 09:53:43 -0300

    Update server.lua

--- VRP/trew_hud_ui/server/server.lua
+++ VRP/trew_hud_ui/server/server.lua
@@ -11,24 +11,30 @@ end
 RegisterServerEvent('trew_hud_ui:getServerInfo')
 AddEventHandler('trew_hud_ui:getServerInfo', function()
 
-	local source = source
+	local source = {source}
 	local playerID = vRP.getUserId(source)
+	local player = vRP.getUserSource({playerID})
 
 	local info = {
-		hunger = vRP.getHunger(playerID),
-		thirst = vRP.getThirst(playerID),
+		hunger = vRP.getHunger({playerID}),
+		thirst = vRP.getThirst({playerID}),
 
-		job = vRP.getUserGroupByType(playerID, 'job'),
+		job = vRP.getUserGroupByType({playerID, 'job'}),
 
-		money = vRP.getMoney(playerID),
-		bankMoney = vRP.getBankMoney(playerID),
-		blackMoney = vRP.getInventoryItemAmount(playerID, Config.vRP.items.blackMoney)
+		money = vRP.getMoney({playerID}),
+		bankMoney = vRP.getBankMoney({playerID}),
+		blackMoney = vRP.getInventoryItemAmount({playerID, Config.vRP.items.blackMoney})
 	}
 	
-	TriggerClientEvent('trew_hud_ui:setInfo', source, info)
+	TriggerClientEvent('trew_hud_ui:setInfo', player, info) 
 end)
 
 RegisterServerEvent('trew_hud_ui:syncCarLights')
 AddEventHandler('trew_hud_ui:syncCarLights', function(status)
-	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, source, status)
+
+	local source = {source}
+	local playerID = vRP.getUserId(source)
+	local player = vRP.getUserSource({playerID})
+
+	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, player, status)
 end)
