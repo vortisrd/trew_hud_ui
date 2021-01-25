5b09f341de36066c0a464ceba6a1e3be3f350c33
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-01-25 23:10:33 +0100

    Removed getServerInfo because of hitch warnings

--- ESX/trew_hud_ui/server/server.lua
+++ ESX/trew_hud_ui/server/server.lua
@@ -1,34 +1,5 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 TriggerEvent('es:addCommand', 'toggleui', function()
 end, { help = _U('toggleui') })
-
-RegisterServerEvent('trew_hud_ui:getServerInfo')
-AddEventHandler('trew_hud_ui:getServerInfo', function()
-
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local job
-
-	if xPlayer ~= nil then
-		if xPlayer.job.label == xPlayer.job.grade_label then
-			job = xPlayer.job.grade_label
-		else
-			job = xPlayer.job.label .. ': ' .. xPlayer.job.grade_label
-		end
-
-		local info = {
-			job = job,
-			money = xPlayer.getMoney(),
-			bankMoney = xPlayer.getAccount('bank').money,
-			blackMoney = xPlayer.getAccount('black_money').money
-		}
-
-		TriggerClientEvent('trew_hud_ui:setInfo', source, info)
-	end
-end)
 
 RegisterServerEvent('trew_hud_ui:syncCarLights')
 AddEventHandler('trew_hud_ui:syncCarLights', function(status)
