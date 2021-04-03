5f77a22d4a27b785b3abdeea407c5d452b64e7ef
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-04-03 19:55:26 +0100

    Fixed error server side :)
    this was omitted because it was without functions also fix an error

--- ESX/trew_hud_ui/server/server.lua
+++ ESX/trew_hud_ui/server/server.lua
@@ -1,6 +1,3 @@
-RegisterCommand('toggleui', function(source, args, user)
-end, { help = _U('toggleui') })
-
 RegisterServerEvent('trew_hud_ui:syncCarLights')
 AddEventHandler('trew_hud_ui:syncCarLights', function(status)
 	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, source, status)
