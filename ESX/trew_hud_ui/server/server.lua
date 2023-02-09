9b9dace256780ed037d0e7c68f0aaf8ad5830e1e
Mycroft <22378232+Mycroft-Studios@users.noreply.github.com>
2023-02-09 21:57:53 +0000

    directory refactor

--- ESX/trew_hud_ui/server/server.lua
+++
@@ -1,4 +0,0 @@
-RegisterServerEvent('trew_hud_ui:syncCarLights')
-AddEventHandler('trew_hud_ui:syncCarLights', function(status)
-	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, source, status)
-end)
---
+++ server/server.lua
@@ -0,0 +1,4 @@
+RegisterServerEvent('trew_hud_ui:syncCarLights')
+AddEventHandler('trew_hud_ui:syncCarLights', function(status)
+	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, source, status)
+end)
